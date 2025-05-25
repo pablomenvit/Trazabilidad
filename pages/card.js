2600ebd904f0480625f57b70734aaa4cf7676737
Pablo Mendez Vita <pmendezvita@gmail.com>
2025-05-25 18:39:36 +0200

    Solcuionamos problema 'undefined' de txHash en card.js

--- pages/card.js
+++ pages/card.js
@@ -7,9 +7,8 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useState } from "react"; 
 
 export default function Card(props) {
-
   const [isCopied, setIsCopied] = useState(false);
- 
+
   const Item = styled(Paper)(({ theme }) => ({
     display: 'flex',
     marginBottom: '8%',
@@ -32,15 +31,16 @@ export default function Card(props) {
     textAlign: 'center',
   }));
 
-  
   const getMyDate = (date) => {
-    var myDate = new Date(parseInt(date));
-    return myDate.getDate() +
+    const myDate = new Date(parseInt(date));
+    return (
+      myDate.getDate() +
       "/" + (myDate.getMonth() + 1) +
       "/" + myDate.getFullYear() +
       " " + myDate.getHours() +
       ":" + myDate.getMinutes() +
-      ":" + myDate.getSeconds();
+      ":" + myDate.getSeconds()
+    );
   };
 
   const translateState = (state) => {
@@ -81,182 +81,185 @@ export default function Card(props) {
     }, 1500);
   };
 
-  
-  const miURL = "https://sepolia.etherscan.io/tx/" + props.data.txHash;
-
-
-  const renderCardContent = () => {
-    const tokenData = props.data;
-
+  // ✅ Validación para evitar errores de prerender
+  if (!props.data || !props.data.txHash) {
     return (
-      <Item>
-        <Title>
-          {translateState(tokenData.operation)}
-        </Title>
-        <div style={{ display: "flex" }}>
-          <Typography variant="subtitle1" color="white" noWrap>
-            <strong>Fecha:</strong>
-          </Typography>
-          <Typography variant="subtitle1" color="white" noWrap>
-            &nbsp;{getMyDate(tokenData.blockTimestamp)}
-          </Typography>
-        </div>
-        {tokenData.operation === 0 ? (
-          <>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Producto:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.attrs.lote}
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;&nbsp;&nbsp;<strong>Fertilizante:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.attrs.fertilizante}
-              </Typography>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Lote Nº:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.attrs.producto}
-              </Typography>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Entidad:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
-              </Typography>
-            </div>
-          </>
-        ) : tokenData.operation === 2 ? (
-          <>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Entidad:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
-              </Typography>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Precio establecido:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.precio} €
-              </Typography>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Repartido de la siguiente manera:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;&nbsp;<strong>Agricultor</strong> &nbsp;{(calculaReparto(tokenData.precio).agricultor).toFixed(2)} €
-                &nbsp;&nbsp;<strong>Transporte</strong> &nbsp;{(calculaReparto(tokenData.precio).transporte).toFixed(2)} €
-                &nbsp;&nbsp;<strong>Comercio</strong> &nbsp;{(calculaReparto(tokenData.precio).comercio).toFixed(2)} €
-              </Typography>
-            </div>
-          </>
-        ) : tokenData.operation === 3 ? (
+      <Typography variant="h6" color="error" style={{ textAlign: 'center', marginTop: '2rem' }}>
+        No hay información disponible para mostrar la tarjeta.
+      </Typography>
+    );
+  }
+
+  const tokenData = props.data;
+  const miURL = "https://sepolia.etherscan.io/tx/" + tokenData.txHash;
+
+  return (
+    <Item>
+      <Title>
+        {translateState(tokenData.operation)}
+      </Title>
+
+      <div style={{ display: "flex" }}>
+        <Typography variant="subtitle1" color="white" noWrap>
+          <strong>Fecha:</strong>
+        </Typography>
+        <Typography variant="subtitle1" color="white" noWrap>
+          &nbsp;{getMyDate(tokenData.blockTimestamp)}
+        </Typography>
+      </div>
+
+      {tokenData.operation === 0 ? (
+        <>
           <div style={{ display: "flex" }}>
             <Typography variant="subtitle1" color="white" noWrap>
-              <strong>Rechazado por:</strong>
+              <strong>Producto:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;{tokenData.attrs.lote}
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;&nbsp;&nbsp;<strong>Fertilizante:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;{tokenData.attrs.fertilizante}
+            </Typography>
+          </div>
+          <div style={{ display: "flex" }}>
+            <Typography variant="subtitle1" color="white" noWrap>
+              <strong>Lote Nº:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;{tokenData.attrs.producto}
+            </Typography>
+          </div>
+          <div style={{ display: "flex" }}>
+            <Typography variant="subtitle1" color="white" noWrap>
+              <strong>Entidad:</strong>
             </Typography>
             <Typography variant="subtitle1" color="white" noWrap>
               &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
             </Typography>
           </div>
-        ) : tokenData.operation === 4 ? (
-          <>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Entidad:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
-              </Typography>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Typography variant="subtitle1" color="white" noWrap>
-                <strong>Se ha transportado entre una temperatura de:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;&nbsp;{tokenData.temperaturaMin}&nbsp;ºC
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;&nbsp;<strong> y de:</strong>
-              </Typography>
-              <Typography variant="subtitle1" color="white" noWrap>
-                &nbsp;&nbsp;{tokenData.temperaturaMax}&nbsp;ºC
-              </Typography>
-            </div>
-          </>
-        ) : tokenData.operation === 5 ? (
+        </>
+      ) : tokenData.operation === 2 ? (
+        <>
           <div style={{ display: "flex" }}>
             <Typography variant="subtitle1" color="white" noWrap>
-              <strong>Producto ofrecido al mercado</strong>
+              <strong>Entidad:</strong>
             </Typography>
             <Typography variant="subtitle1" color="white" noWrap>
-              &nbsp;&nbsp;{tokenData.attrs.lote}
+              &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
             </Typography>
           </div>
-        ) : tokenData.operation === 6 ? (
           <div style={{ display: "flex" }}>
             <Typography variant="subtitle1" color="white" noWrap>
-              <strong>Comprado por:</strong>
+              <strong>Precio establecido:</strong>
             </Typography>
             <Typography variant="subtitle1" color="white" noWrap>
-              &nbsp;&nbsp;{tokenData.user.nombre}
+              &nbsp;{tokenData.precio} €
             </Typography>
           </div>
-        ) : (
           <div style={{ display: "flex" }}>
             <Typography variant="subtitle1" color="white" noWrap>
-              <strong>Entregado a:</strong>
+              <strong>Repartido de la siguiente manera:</strong>
             </Typography>
             <Typography variant="subtitle1" color="white" noWrap>
-              &nbsp;&nbsp;{tokenData.user.nombre}
+              &nbsp;&nbsp;<strong>Agricultor</strong> &nbsp;{(calculaReparto(tokenData.precio).agricultor).toFixed(2)} €
+              &nbsp;&nbsp;<strong>Transporte</strong> &nbsp;{(calculaReparto(tokenData.precio).transporte).toFixed(2)} €
+              &nbsp;&nbsp;<strong>Comercio</strong> &nbsp;{(calculaReparto(tokenData.precio).comercio).toFixed(2)} €
             </Typography>
           </div>
-        )}
+        </>
+      ) : tokenData.operation === 3 ? (
+        <div style={{ display: "flex" }}>
+          <Typography variant="subtitle1" color="white" noWrap>
+            <strong>Rechazado por:</strong>
+          </Typography>
+          <Typography variant="subtitle1" color="white" noWrap>
+            &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
+          </Typography>
+        </div>
+      ) : tokenData.operation === 4 ? (
+        <>
+          <div style={{ display: "flex" }}>
+            <Typography variant="subtitle1" color="white" noWrap>
+              <strong>Entidad:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;{tokenData.user.nombre}, &nbsp;&nbsp;&nbsp;{translateRole(tokenData.user.role)}
+            </Typography>
+          </div>
+          <div style={{ display: "flex" }}>
+            <Typography variant="subtitle1" color="white" noWrap>
+              <strong>Se ha transportado entre una temperatura de:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;&nbsp;{tokenData.temperaturaMin}&nbsp;ºC
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;&nbsp;<strong> y de:</strong>
+            </Typography>
+            <Typography variant="subtitle1" color="white" noWrap>
+              &nbsp;&nbsp;{tokenData.temperaturaMax}&nbsp;ºC
+            </Typography>
+          </div>
+        </>
+      ) : tokenData.operation === 5 ? (
+        <div style={{ display: "flex" }}>
+          <Typography variant="subtitle1" color="white" noWrap>
+            <strong>Producto ofrecido al mercado</strong>
+          </Typography>
+          <Typography variant="subtitle1" color="white" noWrap>
+            &nbsp;&nbsp;{tokenData.attrs.lote}
+          </Typography>
+        </div>
+      ) : tokenData.operation === 6 ? (
+        <div style={{ display: "flex" }}>
+          <Typography variant="subtitle1" color="white" noWrap>
+            <strong>Comprado por:</strong>
+          </Typography>
+          <Typography variant="subtitle1" color="white" noWrap>
+            &nbsp;&nbsp;{tokenData.user.nombre}
+          </Typography>
+        </div>
+      ) : (
+        <div style={{ display: "flex" }}>
+          <Typography variant="subtitle1" color="white" noWrap>
+            <strong>Entregado a:</strong>
+          </Typography>
+          <Typography variant="subtitle1" color="white" noWrap>
+            &nbsp;&nbsp;{tokenData.user.nombre}
+          </Typography>
+        </div>
+      )}
 
-        <Grid>
-          <Typography variant="h6" color="white" component="p">
-            Informacion de la transacción
-          </Typography>
-          <Typography variant="body2" color="white" component="p">
-            Número de bloque: {tokenData.blockNumber}
-          </Typography>
-          <CopyToClipboard text={tokenData.txHash} onCopy={handleCopyClick}>
-            <Button style={{ 'marginTop': '2%', 'marginBottom': '2%' }} variant="primary">
-              <Typography variant="body2" color="white" component="p">
-                {isCopied ? 'Copiado!' : 'Copiar Hash'}
-              </Typography>
-            </Button>
-          </CopyToClipboard>
-          &nbsp;&nbsp;
-          <Button
-            style={{ 'marginTop': '2%', 'marginBottom': '2%' }}
-            variant="primary"
-            href={miURL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <Grid>
+        <Typography variant="h6" color="white" component="p">
+          Información de la transacción
+        </Typography>
+        <Typography variant="body2" color="white" component="p">
+          Número de bloque: {tokenData.blockNumber}
+        </Typography>
+        <CopyToClipboard text={tokenData.txHash} onCopy={handleCopyClick}>
+          <Button style={{ 'marginTop': '2%', 'marginBottom': '2%' }} variant="primary">
             <Typography variant="body2" color="white" component="p">
-              {'Ver en Etherscan'}
+              {isCopied ? '¡Copiado!' : 'Copiar Hash'}
             </Typography>
           </Button>
-        </Grid>
-      </Item>
-    );
-  };
-
- 
-  return renderCardContent();
+        </CopyToClipboard>
+        &nbsp;&nbsp;
+        <Button
+          style={{ 'marginTop': '2%', 'marginBottom': '2%' }}
+          variant="primary"
+          href={miURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Typography variant="body2" color="white" component="p">
+            {'Ver en Etherscan'}
+          </Typography>
+        </Button>
+      </Grid>
+    </Item>
+  );
 }
